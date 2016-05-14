59f2b9c1bcd7b44e90b97d49e28dca3ae3e77ee4
CAR <CAR@CAR-HP>
2016-05-14 23:39:09 +0200

    Quadtree test2 Beginning of Red 5 Tests fails

--- test/quadtree/QuadtreeTest.java
+++ test/quadtree/QuadtreeTest.java
@@ -29,4 +29,22 @@ public class QuadtreeTest {
         assertTrue(node.getNodes().isEmpty());
         assertEquals(node.getPoints().size(), 4);
     }
+    
+    @Test
+    public void addMoreThan4Points(){
+        Node node = new Node(new Point(0,0), 4, 4);
+        node.getPoints().add(new Point(1,1));
+        node.getPoints().add(new Point(1,3));
+        node.getPoints().add(new Point(3,1));
+        node.getPoints().add(new Point(3,2));
+        Quadtree tree = new Quadtree();
+        tree.setGrid(node);
+        tree.addPoint(new Point(2,3));
+        assertEquals(node.getNodes().size(), 4);
+        assertEquals(node.getPoints().size(), 2);
+        assertEquals(node.getNodes().get(0).getPoints().size(), 1);
+        assertEquals(node.getNodes().get(1).getPoints().size(), 1);
+        assertEquals(node.getNodes().get(2).getPoints().size(), 1);
+        assertEquals(node.getNodes().get(3).getPoints().size(), 0);
+    }
 }
